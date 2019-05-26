c34c0406d05fba6f89cabbf28431cc5d7b58eaad
nathanbarton <39756121+nathanbarton@users.noreply.github.com>
2019-05-26 01:07:50 -0700

    updated documentation for motor_control library

--- libraries/motor_control.c
+++ libraries/motor_control.c
@@ -8,8 +8,13 @@
 //   spool robot project.  
 //
 //  The public functions are:
+//		motor_control_init - Initialize IO pins for communicating with DRV8840
+//		motor_control_enable - Enable output of the DRV8840
+//		motor_control_disable - Disable output of the DRV8840
+//		set_motor_current - set the direction and current of the DRV8840 output
 //
-//  Known Bugs:		
+//  Known Bugs/Limitations:	
+//		set_motor_current can only set values prescribed by the DRV8840	
 //
 //  Revision History:
 //     2019-05-25   Nathan T Barton      Initial revision
