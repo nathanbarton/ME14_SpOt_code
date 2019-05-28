05d03f53b47f28fa6bdb7042ae3b1b2300826bba
nathanbarton <39756121+nathanbarton@users.noreply.github.com>
2019-05-28 16:14:14 -0700

    added command received status LED to LED3

--- SpOt_basic_functionality/SpOt_basic_functionality.ino
+++ SpOt_basic_functionality/SpOt_basic_functionality.ino
@@ -16,6 +16,7 @@
 //     2019-05-26   Nathan T Barton      Initial revision
 //     2019-05-28   Nathan T Barton      added motor speed calculation
 //     2019-05-28   Nathan T Barton      refactored motorSpeed to motorCurrent to better reflect purpose
+//     2019-05-28   Nathan T Barton      added command received status LED
 
 #include "SpOt_board.h"
 #include "motor_control.h"
@@ -37,7 +38,8 @@ extern volatile long encoderPosition;
 
 unsigned long lastRefreshTime = 0;   //time since periodic serial output has been refreshed
 bool led_state = false;   //boolean for blinking status LED to show that the code is running
-long lastEncoderPosition = 0;
+long lastEncoderPosition = 0;  //for calculating motor speed
+bool cmd_received = false;  //boolean for blinking command received LED
 
 
 void setup() {
@@ -48,9 +50,10 @@ void setup() {
   
   Serial1.begin(BAUD_RATE);
 
-  //setup status LED
+  //setup status LEDs
   pinMode(PIN_LED_2, OUTPUT);
   digitalWrite(PIN_LED_2, HIGH);
+  pinMode(PIN_LED_3, OUTPUT);
 
   //set up encoder
   encoder_init();
@@ -85,6 +88,9 @@ void loop() {
 
     //update motor current output
     set_motor_current(motorCurrent);
+    
+    //set flag for command received LED
+    cmd_received = true;
   }
 
   //check if time to refresh display
@@ -124,6 +130,18 @@ void loop() {
       led_state = false;
       digitalWrite(PIN_LED_2, LOW);
     }
+
+    //light up command received LED
+    if(cmd_received == true)
+    {
+      digitalWrite(PIN_LED_3, HIGH);
+    }
+    else
+    {
+      digitalWrite(PIN_LED_3, LOW);
+    }
+    //reset command received flag
+    cmd_received = false;
    
     
   }
