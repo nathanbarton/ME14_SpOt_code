b6e557382b3bd10df5314241ecc8921e8a255411
nathanbarton <39756121+nathanbarton@users.noreply.github.com>
2019-06-02 19:53:10 -0700

    updated documentation in spot_functionality

--- SpOt_functionality/SpOt_functionality.ino
+++ SpOt_functionality/SpOt_functionality.ino
@@ -1,3 +1,19 @@
+// ----------------------------------------
+//      SpOt_basic_functionality
+//      ME14 Spring 2019
+//      Author: Nathan T Barton
+// ----------------------------------------
+// This program implements the control loop functionality of the SpOt robot for
+//    ME14.
+//
+//  Revision History:
+//     2019-06-02   Nathan T Barton      Initial revision
+
+#include "SpOt_board.h"
+#include "motor_control.h"
+#include "encoder_position.h"
+
+
 void setup() {
   // put your setup code here, to run once:
 
