4a887bdeb9a3c83cf7df17c84a4a6706202a4ac6
Brit W <wybrit98@gmail.com>
2019-06-04 00:17:46 -0700

    Update pid_loop.h

---
+++ SpOt_functionality/pid_loop.h
@@ -0,0 +1,29 @@
+// ----------------------------------------
+//      pid_loop.h
+//      ME14 Spring 2019
+//      Author: Brittany Wylie
+// ----------------------------------------
+// This file includes function definitions and global variables for using the
+// encoder position for the SpOt board as a process variable for PID control. It
+// is general enough to accept IMU data for this process variable in producing
+// a linear position.
+//
+//  The public functions are:
+//    
+//
+//  Global Variables:
+//    
+//
+
+//Local Includes
+#include <arduino.h>
+
+
+//Program Constants
+#define REFRESH_PERIOD    200   //in ms
+#define KP_INCREMENT      0.01
+#define KI_INCREMENT      0.01
+#define KD_INCREMENT      0.01
+
+// Function Prototype Declarations
+void Compute();
