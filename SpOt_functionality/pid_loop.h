ef4f650811d1690a36535e890dad533625db9f4c
maheckjerez <maheckjerez@gmail.com>
2019-06-04 19:32:50 -0700

    SETPOINT_CAP Defined in header and referenced in body

--- SpOt_functionality/pid_loop.h
+++ SpOt_functionality/pid_loop.h
@@ -29,6 +29,7 @@
 #define KI_INCREMENT      0.01
 #define KD_INCREMENT      0.01
 #define REFRESH_PERIOD 	  200 //ms
+#define SETPOINT_CAP      600 //inches -> 50 ft
 
 #define MANUAL 0
 #define AUTOMATIC 1
